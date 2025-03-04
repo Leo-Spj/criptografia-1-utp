28f4613b309aca2c97d0e80c35ec27291d58a83e
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2025-03-04 17:44:21 -0500

    Agregar configuración para despliegue en GitHub Pages y scripts de predespliegue en package.json

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //gh pages
+  base: 'https://Leo-Spj.github.io/criptografia-1-utp',
 })
